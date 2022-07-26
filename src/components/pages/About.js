4d8acc22d7f2a60148c2dd8919b2c1f9d6d0a139
Arcesio <jav8319@hotmail.com>
2022-07-25 20:34:49 -0400

    photo updated

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,10 +3,11 @@ import image5 from '../img/PHOTO.png';
 
 
 export default function About() {
+  const linkStyle1 = { Height: '80px', Width: '60px' };
   return (
     <div>
       <h1>About me</h1>
-      <img src={image5}  style={linkStyle} alt="decorative"></img>
+      <img src={image5}  style={linkStyle1} alt="decorative"></img>
       <p>
       Front-end Web Developer with strong math skills and a degree in Material Engineering. Recognized as proactive, resourceful, and persistent problem-solver. I’m looking to leverage my development skills to provide small businesses with web applications that support their metrics. I earned a certificate as a full-stack developer from the University of Toronto where I implemented JavaScript, Node, SQL, HTML, and CSS. 
 
